2487763473f0d2925378afdb62a1b646837229e9
lai2-01 <lai2-01@itismeucci.com>
2021-10-25 11:37:09 +0200

    modificata la classe ChatServer

--- src/main/java/chatsocket/ChatServer.java
+++ src/main/java/chatsocket/ChatServer.java
@@ -1,5 +1,15 @@
 package chatsocket;
 
-public class ChatServer {
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class ChatServer extends JFrame {
+    public ChatServer(){
+    super("Chat Server");
+    this.setSize(new Dimension(600,400));
+    this.setLocationRelativeTo(null);
+    this.setBackground(Color.yellow);
     
+    }
 }
