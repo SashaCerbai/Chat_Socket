33daa9919c7abb3368b9970033518890e328e3fa
lai2-01 <lai2-01@itismeucci.com>
2021-11-08 10:26:11 +0100

    modificata classe chat server

--- src/main/java/chatsocket/ChatServer.java
+++ src/main/java/chatsocket/ChatServer.java
@@ -1,15 +1,70 @@
 package chatsocket;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ * Hello world!
+ *
+ */
+public class ChatServer 
+{
 
-import java.awt.Color;
-import java.awt.Dimension;
-import javax.swing.JFrame;
+        private final ServerSocket serverSocket;
+        
+        public ChatServer(ServerSocket ServerSocket){
 
-public class ChatServer extends JFrame {
-    public ChatServer(){
-    super("Chat Server");
-    this.setSize(new Dimension(600,400));
-    this.setLocationRelativeTo(null);
-    this.setBackground(Color.yellow);
+            this.serverSocket = ServerSocket;
+        }
     
+
+        public void startServer(){
+
+            try{
+
+                while(!serverSocket.isClosed()){
+                    // nel client Handler, il server socket sarà chiuso
+                    Socket socket = serverSocket.accept();
+                    System.out.println("Il client si è unito alla chat");
+                    ClientHandler clientHandler = new ClientHandler(socket);
+                    Thread thread = new Thread(clientHandler);
+                    thread.start();
+
+                }
+             } catch (IOException e){
+                    closeServerSocket();
+                }
+            }
+
+                public void closeServerSocket(){
+
+                    try {
+                        
+                        if(serverSocket != null){
+
+                            serverSocket.close();
+                        }
+
+                    } catch (IOException e) {
+                        
+                        e.printStackTrace();
+
+                    }
+                }
+
+            }
+        }
+
+
     }
-}
+    public static void main( String[] args )
+    {
+
+        ServerSocket serverSocket = new ServerSocket(1234);
+        ChatServer server = new ChatServer(serverSocket);
+        server.startServer();
+
+
+    }
+
+
+
